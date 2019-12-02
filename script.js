1d328c244f065e16a69c5a4cecb91c103c382592
Joshua Armoney <armoneyj210@gmail.com>
2019-12-02 13:50:05 -0500

    start of pseudocode

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-// Craft a play button
-// when the play button is clicked create the game board using a for loop
-// create a class for the squares names square
-// as the game board is created add the class square to the tiles
 // Craft an input for the user to enter their name
-//
+
+//  This name should appear in the lower left hand corner after the game is loaded.
+
+// Craft a play button
+
+// create a class for the squares names square
+
+// when the play button is clicked create the game board using a for loop
+
+// There shold be 36 squares; 30 for questions and 6 for categories
+
+// as the game board is created add the class square to the tiles
+
+// also the value of each square should be set using a dataset attribute
+
+// Set the users score to zero
+
+// create click event for the squares that produces a modal with a statement and four questions.
+
+// only one answer can be selected
+
+// create functions for if the user gets the answer right
+
+// create function if the user gets it wrong.
+
+// use click event to trigger these functions when user chooses an option from the modal menu.
+
+// make a function for randomly generating questions from a bank
+
+// conditional for if a player has reached 5,000 pts that displays winner.
+
+// If there are no more quesions and the points total is not at 5,000, display try again
+
+// after either a win or a loss the Play button and user name input should reappear.
