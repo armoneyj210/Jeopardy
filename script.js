70db9dcb1e04c43e334d544d5a0dfaaa56551e8e
Joshua Armoney <armoneyj210@gmail.com>
2019-12-04 16:58:03 -0500

    populating modals need questions

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
-// Craft an input for the user to enter their name
-
-//  This name should appear in the lower left hand corner after the game is loaded.
+// create an array of questions
 
 // Craft a play button
-
 // create a class for the squares names square
-
 // There shold be 36 squares; 30 for questions and 6 for categories
-
 // when the play button is clicked create the game board using a for loop
-
 // as the game board is created add the class square to the tiles
-
 // also the value of each square should be set using a dataset attribute
-let categories = ["Food", "Science", "Music", "Film", "Sports", "Art"];
+
 let playButton = document.querySelector(".playButton");
+
+let questions = [
+  {
+    cat: "Food",
+    answer: "The sound does a doggy makes",
+    solution: [
+      "What is bark?",
+      "What is moo?",
+      "What is meow?",
+      "What is oink?"
+    ],
+    correct: "What is bark",
+    dollar: "$200"
+  },
+  {
+    cat: "Food",
+    answer: "The temperature at which water boils.",
+    solution: [
+      "What is 212 F?",
+      "What is 110 F?",
+      "What is 300 F?",
+      "What is 350 F"
+    ],
+    correct: "What is 212 F?",
+    dollar: "$400"
+  }
+];
 
 for (let i = 0; i < 36; i++) {
   let gameBoard = document.createElement("div");
+  let categories = ["Food", "Science", "Music", "Film", "Sports", "Art"];
   gameBoard.classList.add("hidden");
+  gameBoard.setAttribute("data-target", "#exampleModal");
+  gameBoard.setAttribute("data-toggle", "modal");
   if (i >= 6 && i < 12) {
     gameBoard.textContent = "$200";
     gameBoard.setAttribute(`data-dollar`, "$200");
@@ -38,52 +61,58 @@ for (let i = 0; i < 36; i++) {
   document.body.appendChild(gameBoard).classList.add("square");
   if (i >= 0 && i < 6) {
     gameBoard.textContent = categories[i];
+    gameBoard.removeAttribute("data-target");
     gameBoard.classList.remove("square");
     gameBoard.classList.add("categories");
   }
   if (i % 6 === 0) {
-    gameBoard.setAttribute(`data-cat`, `food`);
+    gameBoard.setAttribute(`data-cat`, `Food`);
   } else if (i === 7 || i === 13 || i === 19 || i === 25 || i == 31) {
-    gameBoard.setAttribute(`data-cat`, `science`);
+    gameBoard.setAttribute(`data-cat`, `Science`);
   } else if (i === 8 || i === 14 || i === 20 || i === 26 || i === 32) {
-    gameBoard.setAttribute(`data-cat`, `music`);
+    gameBoard.setAttribute(`data-cat`, `Music`);
   } else if (i % 3 === 0) {
-    gameBoard.setAttribute(`data-cat`, `film`);
+    gameBoard.setAttribute(`data-cat`, `Film`);
   } else if (i === 10 || i === 16 || i === 22 || i === 28 || i === 34) {
-    gameBoard.setAttribute(`data-cat`, `sport`);
+    gameBoard.setAttribute(`data-cat`, `Sport`);
   } else if (i === 11 || i === 17 || i === 23 || i === 29 || i === 35) {
-    gameBoard.setAttribute(`data-cat`, `art`);
+    gameBoard.setAttribute(`data-cat`, `Art`);
   }
   playButton.addEventListener(`click`, function() {
     playButton.classList.remove("playButton");
     playButton.classList.add("hidden");
     gameBoard.classList.remove("hidden");
   });
+  gameBoard.addEventListener("click", function(evt) {
+    for (let i = 0; i < questions.length; i++) {
+      if (
+        questions[i].cat === this.dataset.cat &&
+        questions[i].dollar === this.dataset.dollar
+      ) {
+        let answerText = document.querySelector(".modal-answer");
+        let title = document.querySelector(".modal-title");
+        let solution1 = document.querySelector('[data-solution="1"]');
+        let solution2 = document.querySelector('[data-solution="2"]');
+        let solution3 = document.querySelector('[data-solution="3"]');
+        let solution4 = document.querySelector('[data-solution="4"]');
+
+        answerText.textContent = questions[i].answer;
+        title.textContent = `${questions[i].cat} - ${questions[i].dollar}`;
+        solution1.textContent = questions[i].solution[0];
+        solution2.textContent = questions[i].solution[1];
+        solution3.textContent = questions[i].solution[2];
+        solution4.textContent = questions[i].solution[3];
+      }
+    }
+  });
 }
+// let squares = document.querySelectorAll(".square");
 
 let food200 = document.querySelector(`[data-dollar='$200'][data-cat='food']`);
-food200.setAttribute("data-target", "#exampleModal");
-food200.setAttribute("data-toggle", "modal");
 let food400 = document.querySelector(`[data-dollar='$400'][data-cat='food']`);
 let food600 = document.querySelector(`[data-dollar='$600'][data-cat='food']`);
 let food800 = document.querySelector(`[data-dollar='$800'][data-cat='food']`);
 let food1000 = document.querySelector(`[data-dollar='$1000'][data-cat='food']`);
-
-food200.addEventListener("click", function() {
-  alert("You clicked on food for $200");
-});
-food400.addEventListener("click", function() {
-  alert("You clicked on food for $400");
-});
-food600.addEventListener("click", function() {
-  alert("You clicked on food for $600");
-});
-food800.addEventListener("click", function() {
-  alert("You clicked on food for $800");
-});
-food1000.addEventListener("click", function() {
-  alert("You clicked on food for $200");
-});
 
 let sci200 = document.querySelector(`[data-dollar='$200'][data-cat='science']`);
 let sci400 = document.querySelector(`[data-dollar='$400'][data-cat='science']`);
@@ -92,21 +121,6 @@ let sci800 = document.querySelector(`[data-dollar='$800'][data-cat='science']`);
 let sci1000 = document.querySelector(
   `[data-dollar='$1000'][data-cat='science']`
 );
-sci200.addEventListener("click", function() {
-  alert("You clicked on science for $200");
-});
-sci400.addEventListener("click", function() {
-  alert("You clicked on science for $400");
-});
-sci600.addEventListener("click", function() {
-  alert("You clicked on science for $600");
-});
-sci800.addEventListener("click", function() {
-  alert("You clicked on science for $800");
-});
-sci1000.addEventListener("click", function() {
-  alert("You clicked on science for $1000");
-});
 
 let mus200 = document.querySelector(`[data-dollar='$200'][data-cat='music']`);
 let mus400 = document.querySelector(`[data-dollar='$400'][data-cat='music']`);
@@ -114,43 +128,11 @@ let mus600 = document.querySelector(`[data-dollar='$600'][data-cat='music']`);
 let mus800 = document.querySelector(`[data-dollar='$800'][data-cat='music']`);
 let mus1000 = document.querySelector(`[data-dollar='$1000'][data-cat='music']`);
 
-mus200.addEventListener("click", function() {
-  alert("You clicked on music for $200");
-});
-mus400.addEventListener("click", function() {
-  alert("You clicked on music for $400");
-});
-mus600.addEventListener("click", function() {
-  alert("You clicked on music for $600");
-});
-mus800.addEventListener("click", function() {
-  alert("You clicked on music for $800");
-});
-mus1000.addEventListener("click", function() {
-  alert("You clicked on music for $1000");
-});
-
 let film200 = document.querySelector(`[data-dollar='$200'][data-cat='film']`);
 let film400 = document.querySelector(`[data-dollar='$400'][data-cat='film']`);
 let film600 = document.querySelector(`[data-dollar='$600'][data-cat='film']`);
 let film800 = document.querySelector(`[data-dollar='$800'][data-cat='film']`);
 let film1000 = document.querySelector(`[data-dollar='$1000'][data-cat='film']`);
-
-film200.addEventListener("click", function() {
-  alert("You clicked on film for $200");
-});
-film400.addEventListener("click", function() {
-  alert("You clicked on film for $400");
-});
-film600.addEventListener("click", function() {
-  alert("You clicked on film for $600");
-});
-film800.addEventListener("click", function() {
-  alert("You clicked on film for $800");
-});
-film1000.addEventListener("click", function() {
-  alert("You clicked on film for $1000");
-});
 
 let sport200 = document.querySelector(`[data-dollar='$200'][data-cat='sport']`);
 let sport400 = document.querySelector(`[data-dollar='$400'][data-cat='sport']`);
@@ -160,43 +142,12 @@ let sport1000 = document.querySelector(
   `[data-dollar='$1000'][data-cat='sport']`
 );
 
-sport200.addEventListener("click", function() {
-  alert("You clicked on sports for $200");
-});
-sport400.addEventListener("click", function() {
-  alert("You clicked on sports for $400");
-});
-sport600.addEventListener("click", function() {
-  alert("You clicked on sports for $600");
-});
-sport800.addEventListener("click", function() {
-  alert("You clicked on sports for $800");
-});
-sport1000.addEventListener("click", function() {
-  alert("You clicked on sports for $1000");
-});
-
 let art200 = document.querySelector(`[data-dollar='$200'][data-cat='art']`);
 let art400 = document.querySelector(`[data-dollar='$400'][data-cat='art']`);
 let art600 = document.querySelector(`[data-dollar='$600'][data-cat='art']`);
 let art800 = document.querySelector(`[data-dollar='$800'][data-cat='art']`);
 let art1000 = document.querySelector(`[data-dollar='$1000'][data-cat='art']`);
 
-art200.addEventListener("click", function() {
-  alert("You clicked on art for $200");
-});
-art400.addEventListener("click", function() {
-  alert("You clicked on art for $400");
-});
-art600.addEventListener("click", function() {
-  alert("You clicked on art for $600");
-});
-art800.addEventListener("click", function() {
-  alert("You clicked on art for $800");
-});
-art1000.addEventListener("click", function() {
-  alert("You clicked on art for $1000");
-});
 // Set the users score to zero
 
 // create click event for the squares that produces a modal with a statement and four questions.
